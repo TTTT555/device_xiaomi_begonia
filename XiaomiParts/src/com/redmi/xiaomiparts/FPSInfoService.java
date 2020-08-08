bf0e1c60d64768dfa4e172a118dc6a0d46a0c225
Flamefusion <meshekharbehera613@gmail.com>
2020-08-08 11:29:02 +0530

    [TMP]begonia: XiaomiParts: changing fps param amd Rebranding

--- XiaomiParts/src/com/redmi/xiaomiparts/FPSInfoService.java
+++ XiaomiParts/src/com/redmi/xiaomiparts/FPSInfoService.java
@@ -57,7 +57,7 @@ public class FPSInfoService extends Service {
     private final String TAG = "FPSInfoService";
     private String mFps = null;
 
-    private static final String MEASURED_FPS = "/sys/devices/virtual/graphics/fb0/measured_fps";
+    private static final String MEASURED_FPS = "/sys/module/primary_display/parameters/display_framerate_main";
 
     private IDreamManager mDreamManager;
 
