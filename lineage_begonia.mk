fe38586d4b727b1676ddce55c1a802fb1adc0588
TTTT555 <albukerko@gmail.com>
2021-01-03 22:17:20 +0000

    begonia: Camera: Add trCam

--- lineage_begonia.mk
+++ lineage_begonia.mk
@@ -25,6 +25,7 @@ TARGET_INCLUDE_STOCK_ARCORE := true
 TARGET_SUPPORTS_GOOGLE_RECORDER := true
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 $(call inherit-product, vendor/ANXCamera/config.mk)
+$(call inherit-product, vendor/trCam/config.mk)
 
 # Screen density
 PRODUCT_AAPT_CONFIG := normal
