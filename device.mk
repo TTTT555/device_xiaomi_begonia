3c29108ba2ab8ef3a12bb10f22c063797306e0fb
Kshitij Gupta <kshitijgm@gmail.com>
2021-01-03 16:35:05 +0100

    begonia: Require libunwindstack for libarmnn
    - Fixes missing symbol __cxa_demangle
    
    Change-Id: Ie8e67ead638d8756a680ceae205ae57520afc4b9

--- device.mk
+++ device.mk
@@ -343,7 +343,8 @@ PRODUCT_PACKAGES += \
     libhwbinder.vendor \
     libhidltransport \
     libhidltransport.vendor \
-    libhidlbase
+    libhidlbase \
+    libunwindstack.vendor
 
 # MTK Jars (for IMS)
 PRODUCT_BOOT_JARS += \
