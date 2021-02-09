26747de33df918fc38ec383b5e87c9515f8ad6cb
TTTT555 <albukerko@gmail.com>
2021-02-09 17:38:16 +0000

    begonia: WFD: Build non-chromecast Wi-Fi Display (1\2)

--- device.mk
+++ device.mk
@@ -285,6 +285,9 @@ PRODUCT_PACKAGES += \
     PixelLiveWallpaperPrebuilt
 
 # WiFi
+PRODUCT_PACKAGES += \
+    libnl
+
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/wifi/p2p_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/p2p_supplicant_overlay.conf \
     $(LOCAL_PATH)/configs/wifi/wpa_supplicant.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/wpa_supplicant.conf \
