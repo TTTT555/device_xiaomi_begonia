89a54667fc84d8893351c30087224c9a4ec0b77b
TTTT555 <albukerko@gmail.com>
2022-01-02 21:36:16 +0000

    xiaomiparts: Add debug.sf.latch_unsignaled switch

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -16,7 +16,6 @@ PRODUCT_PROPERTY_OVERRIDES +=  \
     debug.mediatek.game_pq_enable=1 \
     debug.mtk_tflite.target_nnapi=29 \
     debug.sf.disable_backpressure=1 \
-    debug.sf.latch_unsignaled=1 \
     drm.service.enabled=true \
     init.userspace_reboot.is_supported=true \
     persist.adb.nonblocking_ffs=0 \
