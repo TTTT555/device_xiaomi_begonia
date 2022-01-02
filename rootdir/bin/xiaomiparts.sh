e8dadb58d4d33a9aefeb2f30a594621cca4b0221
TTTT555 <albukerko@gmail.com>
2022-01-02 23:17:22 +0000

    xiaomiparts: Add OMX priority switch

--- rootdir/bin/xiaomiparts.sh
+++ rootdir/bin/xiaomiparts.sh
@@ -5,6 +5,7 @@ sprofileold=10
 tcprofileold=10
 vsyncdisold=10
 latch_unsignaled_old=10
+codecs_old=10
 
 # loop, run every 3 seconds
 while true
@@ -111,6 +112,25 @@ if [ "$latch_unsignaled_old" != "$latch_unsignaled" ]; then
   ;;
   esac
 	latch_unsignaled_old=$latch_unsignaled
+fi
+
+## Codecs priority
+codecs="$(getprop persist.xp.hw_codecs)"
+if [ "$codecs_old" != "$codecs" ]; then
+  case $codecs in
+  0)# MTK HW
+  setprop debug.stagefright.omx_default_rank 0
+  killall mediaserver
+  ;;
+  1)# Google OMX
+  setprop debug.stagefright.omx_default_rank 1000
+  killall mediaserver
+  ;;
+  *)# First boot params
+  setprop debug.stagefright.omx_default_rank 0
+  ;;
+  esac
+	codecs_old=$codecs
 fi
 
 sleep 3
