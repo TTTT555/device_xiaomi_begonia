f878caf023537c42585aaeebeaf92561c71c84cc
TTTT555 <albukerko@gmail.com>
2022-04-28 01:43:06 +0200

    Add JamesDSPmanager

--- lineage_begonia.mk
+++ lineage_begonia.mk
@@ -26,6 +26,7 @@ TARGET_SUPPORTS_GOOGLE_RECORDER := true
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 $(call inherit-product, vendor/ANXCamera/config.mk)
 $(call inherit-product, vendor/burial8/config.mk)
+$(call inherit-product, vendor/JamesDSPManager/config.mk)
 $(call inherit-product, vendor/viper4androidFX/config.mk)
 $(call inherit-product, vendor/dirac/config.mk)
 
