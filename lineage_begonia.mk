2bf23c835a5b9352d7e0f7f0a7992d0308430876
TTTT555 <albukerko@gmail.com>
2022-04-30 00:48:54 +0200

    BegoniaParts: Add Camera's switch  * And pick up G-cam 64

--- lineage_begonia.mk
+++ lineage_begonia.mk
@@ -27,6 +27,7 @@ $(call inherit-product, vendor/viper4androidFX/config.mk)
 $(call inherit-product, vendor/dirac/config.mk)
 $(call inherit-product, vendor/ANXCamera/config.mk)
 $(call inherit-product, vendor/burial8/config.mk)
+$(call inherit-product, vendor/gcam64begonia/config.mk)
 $(call inherit-product, vendor/JamesDSPManager/config.mk)
 $(call inherit-product, vendor/dlb/config.mk)
 
